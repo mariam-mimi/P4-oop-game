385139ca41d0baae3cd4e3526f2d9e9d03488711
mariam-mimi <mariam.kisto.06@icloud.com>
2024-02-18 17:40:50 -0500

    app.js updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+buttons = document.querySelectorAll(".key");
+
+document.getElementById("btn__reset").addEventListener("click", () => {
+    game.startGame();
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        game.handleInteraction(event.target);
+    });
+});
+
+document.addEventListener("keydown", (e) => {
+    const keyClicked = e.key.toLowerCase();
+    const lettersRegex = /^[a-z]$/;
+    if (lettersRegex.test(keyClicked)) {
+        buttons.forEach(button => {
+            if (button.textContent === keyClicked) {
+                game.handleInteraction(button);
+            }
+        });
+    }
+});
